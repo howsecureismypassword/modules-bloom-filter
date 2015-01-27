45998c248bf0db95a72380025ab4ce6239533d85
Mark Wales <mark@smallhadroncollider.com>
2015-01-27 11:33:14 +0000

    added config file

--- create.js
+++ create.js
@@ -1,12 +1,13 @@
 "use strict";
 
 var fs = require("fs");
+var config = require("./config");
 var passes = require("./passwords");
 var Bloom = require("bloomfilter").BloomFilter;
 var R = require("ramda");
 
 passes(function (err, passes) {
-    var bloom = new Bloom(9 * passes.length, 16);
+    var bloom = new Bloom(9 * passes.length, config.hashes);
     R.forEach(R.bind(bloom.add, bloom), passes);
     fs.writeFile(__dirname + "/bloom.json", JSON.stringify([].slice.call(bloom.buckets)));
 });
--- exists.js
+++ exists.js
@@ -2,8 +2,9 @@
 
 var Bloom = require("bloomfilter").BloomFilter;
 var filter = require("./bloom");
+var config = require("./config");
 
-var tester = new Bloom(filter, 16);
+var tester = new Bloom(filter, config.hashes);
 
 module.exports = function (text) {
     return tester.test(text);
